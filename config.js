829d42336138f8ba6ea41fbd71cb292e5fe4f03b
lialia06 <rachrisgamer@gmail.com>
2022-09-14 04:10:57 +0700

    LiaGoodnessBot-V1

---
+++ config.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'piceg',
+}
+
+//api
+global.xteam = 'apivproject'
+global.lolhuman = 'rey2k21'
+
+// Other
+global.owner = ['6288239560059']
+global.premium = ['6288239560059']
+global.ownername = 'Yans'
+global.botname = 'LiaGoodnessBot'
+global.packname = '© Yanss'
+global.gc = 'Vipul XL:085965981742' 
+global.linkyt = 'https://youtube.com/'
+global.linkgc = 'https://chat.whatsapp.com/'
+global.limitawal = '10'
+global.author = '@yans\n@LiaGoodnessBot'
+global.sessionName = 'sessionya'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⌕'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: '*Tunggu bentar tod*_, lagi di proses_',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
+global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
